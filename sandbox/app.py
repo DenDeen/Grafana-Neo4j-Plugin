414f4d0dbb8482b58d6eccd6b39cfc49b3d3f4f9
= <=>
2021-10-03 13:32:44 +0200

    removed venv and added it to .gitignore

--- sandbox/app.py
+++ sandbox/app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template
 from py2neo import Graph
 import pandas as pd
@@ -13,16 +12,18 @@ DB_PW = os.environ['MY_PASSWORD']
 DB_NAME = os.environ['DATABASE_NAME']
 
 graph = Graph(
-host=DN_HOST,
-port=DB_PORT,
-user=DB_USERNAME,
-password=DB_PW,
-name=DB_NAME
+  host=DN_HOST,
+  port=DB_PORT,
+  user=DB_USERNAME,
+  password=DB_PW,
+  name=DB_NAME
 )
+
 
 @app.route('/')
 def index():
   return 'Mikkel\'s API that converts Neo4j dataresponses to the right format for Grafana.'
+
 
 @app.route('/nodes')
 def nodes_without_label():
@@ -33,6 +34,7 @@ def nodes_without_label():
   
   return json.dumps([row.dropna().to_dict() for index,row in df.iterrows()])
 
+
 @app.route('/nodes/<label>')
 def nodes_with_label(label):
   schema_query = "MATCH (n:%s) RETURN n, id(n) as id" % label.capitalize()
@@ -42,12 +44,14 @@ def nodes_with_label(label):
   
   return json.dumps([row.dropna().to_dict() for index,row in df.iterrows()])
 
+
 @app.route('/edges')
 def edges_without_label():
   schema_query = "MATCH (n)-[r]->(m) RETURN type(r) AS mainStat, id(r) as id, id(n) as source, id(m) as target"
   df = graph.run(schema_query).to_data_frame()
 
   return json.dumps([row.to_dict() for index,row in df.iterrows()])
+
 
 @app.route('/edges/<label>')
 def edges_with_label(label):
